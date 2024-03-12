d412e9bb2a42c695abc98e99ff6e8ee7c0cd6ec6
Pazoraf <alexander.flaherty@hotmail.com>
2024-03-12 16:32:20 +0000

    Fixed project display bug

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import './nav.css'
 
 function NavbarComponent() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <div className="container-fluid">
-        <Navbar.Brand href="#home">Alexander Flaherty Portfolio</Navbar.Brand>
+    <Navbar expand="lg" className="bg-body-tertiary ">
+      <div className="container-fluid navbar">
+        <Navbar.Brand href="/">Alexander Flaherty Portfolio</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="Projects">Projects</Nav.Link>
-            <Nav.Link href="#link">About me</Nav.Link>
             <Nav.Link href="#link">Contact me</Nav.Link>
-            <NavDropdown title="Socials" id="basic-nav-dropdown">
-            </NavDropdown>
           </Nav>
         </Navbar.Collapse>
       </div>
--- src/components/pages/Project Gallery/ProjectGallery.jsx
+++ src/components/pages/Project Gallery/ProjectGallery.jsx
@@ -5,12 +5,19 @@ import './ProjectGallery.css';
 function ProjectGallery(props) {
   return (
     <div className="row d-flex">
-      <Card className="vertical-card col">
-        <Card.Img variant="top" src={props.image} className="img-fluid" alt="Card image" />
-        <Card.Body>
-          <Card.Title>{props.name}</Card.Title>
-        </Card.Body>
-      </Card>
+    <Card className="vertical-card col">
+      <Card.Img variant="top" src={props.image} />
+      <Card.Body>
+        <Card.Title>{props.name}</Card.Title>
+        <Card.Text>
+          {props.description}
+        </Card.Text>
+      </Card.Body>
+      <Card.Body>
+        <Card.Link href={props.deploy}>Link to deployment</Card.Link>
+        <Card.Link href={props.github}>Link to GitHub repo.</Card.Link>
+      </Card.Body>
+    </Card>
     </div>
   );
 }
