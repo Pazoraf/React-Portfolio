499e7abdb111151ecaef0bc65205488b140232f1
Pazoraf <alexander.flaherty@hotmail.com>
2024-03-12 14:07:59 +0000

    Added home and project pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
-import { React, useState } from 'react'
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComponent from './components/nav'
+import Home from './components/pages/home/home';
+import ProjectComponent from './components/pages/Project Gallery/ProjectComponent';
+import projects from './projects.json'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <NavbarComponent />
+    <Router>
+      <NavbarComponent />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="Projects" element={
+          projects.map((projects) => (
+            <ProjectComponent
+            id={projects.id}
+            name={projects.name}
+            description={projects.description}
+            github={projects.github}
+            deployed={projects.deployed}
+            image={projects.image}
+            />
+          ))
+        } />
+      </Routes>
+    </Router>
+
   )
 }
 
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -8,12 +9,14 @@ function NavbarComponent() {
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
         <Navbar.Brand href="#home">Alexander Flaherty Portfolio</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="Projects">Projects</Nav.Link>
+            <Nav.Link href="#link">About me</Nav.Link>
+            <Nav.Link href="#link">Contact me</Nav.Link>
+            <NavDropdown title="Socials" id="basic-nav-dropdown">
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
